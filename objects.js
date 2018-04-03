35c88eb9504707ce7db7d6a8eb62b9e2d5f5e4b3
Drin K <drinkaziu@gmail.com>
2018-04-03 16:15:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,13 +8,7 @@ function updatePlaylist (obj, key, value) {
    return obj;
 }
 
-function removeFromPlaylist (obj, key) {
-  delete playlist["My Bloody Valentine"];
-  delete playlist['Phil Ochs'];
-  delete playlist["Dave Matthews"];
-  delete playlist["Slowdive"];
-  return obj;
-}
+
 
 
 
